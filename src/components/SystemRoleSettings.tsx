a717bebd8523e6917e450b9e4b082413fbaafa2d
graceladia161 <graceladia161@gmail.com>
2023-08-02 01:29:13 +0900

    Rollback to suggestions popup

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -1,25 +1,56 @@
-import { Show } from 'solid-js'
-import type { Accessor, Setter } from 'solid-js'
-import IconEnv from './icons/Env'
+import { createSignal, Show, onCleanup } from 'solid-js';
+import type { Accessor, Setter } from 'solid-js';
+import IconEnv from './icons/Env';
 
 interface Props {
-  canEdit: Accessor<boolean>
-  systemRoleEditing: Accessor<boolean>
-  setSystemRoleEditing: Setter<boolean>
-  currentSystemRoleSettings: Accessor<string>
-  setCurrentSystemRoleSettings: Setter<string>
+  canEdit: Accessor<boolean>;
+  systemRoleEditing: Accessor<boolean>;
+  setSystemRoleEditing: Setter<boolean>;
+  currentSystemRoleSettings: Accessor<string>;
+  setCurrentSystemRoleSettings: Setter<string>;
 }
 
 export default (props: Props) => {
-  let systemInputRef: HTMLTextAreaElement
+  let systemInputRef: HTMLTextAreaElement;
+
+  const [showSuggestions, setShowSuggestions] = createSignal(false);
+  const suggestions = ["Suggestion 1", "Suggestion 2", "Suggestion 3","Suggestion 1", "Suggestion 2", "Suggestion 3","Suggestion 1", "Suggestion 2", "Suggestion 3","Suggestion 1", "Suggestion 2", "Suggestion 3"]; // Replace this with your actual suggestions
+
+  const handleInput = (e) => {
+    if (e.target.value.slice(-1) === "/") {
+      setShowSuggestions(true);
+    } else {
+      setShowSuggestions(false);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    systemInputRef.value = systemInputRef.value.slice(0, -1) + suggestion; // Replace the "/" with the suggestion
+    setShowSuggestions(false);
+  };
 
   const handleButtonClick = () => {
-    props.setCurrentSystemRoleSettings(systemInputRef.value)
-    props.setSystemRoleEditing(false)
-  }
+    props.setCurrentSystemRoleSettings(systemInputRef.value);
+    props.setSystemRoleEditing(false);
+  };
 
   return (
     <div class="my-4">
+      <style>
+        {`
+          .suggestion-popup {
+            position: absolute;
+            z-index: 1;
+            max-height: 200px;
+            overflow-y: auto;
+            background-color: white;
+            border: 1px solid #ccc;
+            padding: 10px;
+            /* Add additional styling as needed */
+          }
+        `}
+      </style>
+
       <Show when={!props.systemRoleEditing()}>
         <Show when={props.currentSystemRoleSettings()}>
           <div>
@@ -41,20 +72,31 @@ export default (props: Props) => {
       </Show>
       <Show when={props.systemRoleEditing() && props.canEdit()}>
         <div>
-          <div class="fi gap-1 op-50 dark:op-60">
+
+        <div class="fi gap-1 op-50 dark:op-60">
             <IconEnv />
             <span>System Role:</span>
           </div>
-          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Gently instruct the assistant and set the behavior of the assistant.</p>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Kindly instruct the assistant and set the behavior of the assistant.</p>
           <div>
             <textarea
               ref={systemInputRef!}
+              onInput={handleInput}
               placeholder="You are a helpful assistant, answer as concisely as possible...."
               autocomplete="off"
               autofocus
               rows="3"
               gen-textarea
             />
+            <Show when={showSuggestions()}>
+              <div class="suggestion-popup">
+                {suggestions.map((suggestion, index) => (
+                  <div key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                    {suggestion}
+                  </div>
+                ))}
+              </div>
+            </Show>
           </div>
           <button onClick={handleButtonClick} gen-slate-btn>
             Set
@@ -62,5 +104,5 @@ export default (props: Props) => {
         </div>
       </Show>
     </div>
-  )
-}
+  );
+};
