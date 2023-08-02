39d1aff97cf3c12fbb7ae4f7ef954341d54abf4b
graceladia161 <graceladia161@gmail.com>
2023-08-02 16:17:15 +0900

    upload Button

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -67,6 +67,13 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
     return ''
   }
 
+
+  // This function will be called when the user selects an image.
+  const handleImageUpload = (event: Event) => {
+    const file = (event.target as HTMLInputElement).files[0];
+    console.log(file);  // For now, we'll just log the selected file.
+  };
+
   return (
     <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
@@ -79,8 +86,12 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
             <IconRefresh />
             <span>Regenerate</span>
           </div>
+
+      {role === 'user' && (
+        <div>
+          <input type="file" accept="image/*" onInput={handleImageUpload} />
         </div>
       )}
     </div>
-  )
-}
+  );
+};
