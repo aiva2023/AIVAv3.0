b97c8d368af33a28d752a340d08d5cb740a190ab
graceladia161 <graceladia161@gmail.com>
2023-08-05 00:50:51 +0900

    gen2

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -170,7 +170,7 @@ export default () => {
     <div my-6>
       { !(messageList().length || currentSystemRoleSettings()) && (
         <div>
-          <p>Welcome! Send your first message to start or choose from the suggestions below:</p>
+          <p>Ready to chat? Send your first message or pick a behavior for AIVA from the suggestions below to get started! 🚀</p>
           <div class="button-container">
             {presetMessages.map(({category, messages}) => (
               <div>
@@ -201,7 +201,7 @@ export default () => {
               ))}
             </div>
           }
-          <p>You can also browse from 100+ templates by typing "/" or set your own AIVA Persona here ⏬</p>
+          <p>Browse 150+ templates by typing "/", or set your own AIVA Persona here! ⏬ <p>
         </div>
       )}
 
